e82eb46f23c335df2bfda3794bfefaca0fd51959
urmilshroff <urmilshroff@gmail.com>
2019-03-01 17:49:57 +0530

    Added option to upload (push) files

--- easygit.sh
+++ easygit.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "Welcome to EasyGit!"
+echo -e "\nWhat would you like to do?\n1. Upload"
+read choice
+
+upload()
+{
+    git add .
+    echo "What changes did you make since the last time you uploaded?"
+    read commit_message
+    git commit -m "$commit_message"
+    git push
+    echo "Successfully uploaded changed files!"
+    echo "Hi"
+}
+
+case $choice in
+    1)
+        upload
+        ;;
+esac
