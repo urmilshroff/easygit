bcdc69e1ec27fca6b96793c12ac5fb0bf38e0fd4
urmilshroff <urmilshroff@gmail.com>
2019-03-28 16:33:27 +0530

    New stuff

--- easygit.sh
+++ easygit.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-YELLOW="\033[0;33m"
-NC="\033[0m"
+create_vars()
+{
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"
+    YELLOW="\033[0;33m"
+    NC="\033[0m"
+    
+    current_branch=$(git symbolic-ref -q HEAD)
+    current_branch=${current_branch##refs/heads/}
+}
 
 debug()
 {
-    echo -e "\nDebug output starts"
-    echo -e "${RED}hello hi i am RED${NC}"
-    echo -e "And I am ${GREEN}GREEN${NC} and ${YELLOW}YELLOW${NC}!"
-    echo -e "\nDebug output ends"
+    echo -e "Debug output starts"
+    # echo -e "${RED}hello hi i am RED${NC}"
+    # echo -e "And I am ${GREEN}GREEN${NC} and ${YELLOW}YELLOW${NC}!"
+    echo -e "Debug output ends"
 }
 
 upload()
 {
     git add .
-    echo -e "\nWhat changes did you make since the last time you uploaded?"
+    echo -e "What changes did you make since the last time you uploaded?"
     read commit_message
-    git commit -m "$commit_message"
+    git commit -m "${commit_message}"
     git push
 
-    echo -e "\n${GREEN}Successfully uploaded all changes (if any!)${NC}"
+    echo -e "${GREEN}Successfully uploaded all changes (if any!)${NC}"
 }
 
 download()
@@ -29,7 +35,7 @@ download()
     git fetch
     git pull
 
-    echo -e "\n${GREEN}Successfully downloaded all changes (if any!)${NC}"
+    echo -e "${GREEN}Successfully downloaded all changes (if any!)${NC}"
 }
 
 refresh()
@@ -43,28 +49,50 @@ refresh()
     download
 }
 
+create_branch()
+{
+    echo -e "Enter name of the new branch. It will be the same as '${current_branch}'"
+    read new_branch
+    git checkout -b ${new_branch}
+    git push --set-upstream origin ${new_branch}
+    current_branch = new_branch
+    
+    echo -e "${GREEN}Successfully created new branch '${current_branch}'!${NC}"
+}
 
-echo -e "\nWhat would you like to do?"
-echo -e "1. Refresh everything"
-echo -e "2. Upload all changes"
-echo -e "3. Download new changes"
+menu()
+{
+    create_vars
+    
+    echo -e "What would you like to do?"
+    echo -e "1. Refresh everything"
+    echo -e "2. Upload all changes"
+    echo -e "3. Download new changes"
+    echo -e "4. Create new branch same as '${current_branch}'"
 
-read choice
+    read choice
 
-case $choice in
-    1)
-        refresh
+    case $choice in
+        1)
+            refresh
+            ;;
+
+        2)
+            upload
+            ;;
+
+        3)
+            download
+            ;;
+            
+        4)
+        create_branch
         ;;
 
-    2)
-        upload
-        ;;
+        *)
+            debug
+            ;;
+    esac
+}
 
-    3)
-        download
-        ;;
-
-    *)
-        debug
-        ;;
-esac
+menu
