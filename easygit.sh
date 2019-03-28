b7c3c3798851d53e805ac37819131c76f7b9bbf0
urmilshroff <urmilshroff@gmail.com>
2019-03-28 14:13:45 +0530

    Added some color

--- easygit.sh
+++ easygit.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+NC="\033[0m"
+
 debug()
 {
     echo -e "\nDebug output starts"
-    echo $is_success
-    # echo $backup
+    echo -e "${RED}hello hi i am RED${NC}"
+    echo -e "And I am ${GREEN}GREEN${NC} and ${YELLOW}YELLOW${NC}!"
     echo -e "\nDebug output ends"
 }
 
 upload()
 {
     git add .
-
     echo -e "\nWhat changes did you make since the last time you uploaded?"
     read commit_message
     git commit -m "$commit_message"
     git push
 
-    echo -e "\nSuccessfully uploaded all changes (if any!)"
+    echo -e "\n${GREEN}Successfully uploaded all changes (if any!)${NC}"
 }
 
 download()
 {
     git fetch
     git pull
-    
-    echo -e "\nSuccessfully downloaded all changes (if any!)"
+
+    echo -e "\n${GREEN}Successfully downloaded all changes (if any!)${NC}"
 }
 
 refresh()
@@ -34,7 +38,7 @@ refresh()
     # do
     #     echo "Output is "
     # done
-    
+
     upload
     download
 }
@@ -58,5 +62,9 @@ case $choice in
 
     3)
         download
+        ;;
+
+    *)
+        debug
         ;;
 esac
